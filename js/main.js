d7ddb18e3552ee86ee6285acbb80a2d7c0e5807c
windspinks <windspinks@gmail.com>
2019-03-07 17:51:56 -0500

    nav dropdown

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-let navButton = document.getElementsByClassName("nav-button")[0];
+let navButton = document.querySelector('.nav-button');
+let nav = document.querySelector('nav');
 
-navButton.addEventListener("click", function(e) {
-  this.classList.toggle("menu-active");
-  let menuLinks = this.nextElementSibling;
-  if (menuLinks.style.maxHeight) {
+let navMenuIsLocked = false;
+
+navButton.addEventListener('mouseover', function(e) {
+  if (!navMenuIsLocked) {
+    this.classList.toggle('menu-active');
+    let menuLinks = this.nextElementSibling;
+    menuLinks.style.maxHeight = menuLinks.scrollHeight + 'px';
+  }
+});
+
+navButton.addEventListener('click', function(e) {
+  navMenuIsLocked = !navMenuIsLocked
+});
+
+nav.addEventListener('mouseleave', function(e) {
+  if (!navMenuIsLocked) {
+    let menuLinks = navButton.nextElementSibling;
+    navButton.classList.toggle('menu-active');
     menuLinks.style.maxHeight = null;
-  } else {
-    menuLinks.style.maxHeight = menuLinks.scrollHeight + "px";
   }
 });
