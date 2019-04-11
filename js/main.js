0ff3b69397de92fca3ed336af47c6b2ea1bf4edb
windspinks <windspinks@gmail.com>
2019-04-11 16:03:19 -0400

    check for user touching

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 let navButton = document.querySelector('.nav-button');
 let nav = document.querySelector('nav');
+//Test for mobile/small window, don't allow menu locking on touchscreen
+window.addEventListener('touchstart', function onFirstTouch() {
+  window.USER_IS_TOUCHING = true;
+  // we only need to know once that a human touched the screen, so we can stop listening now
+  window.removeEventListener('touchstart', onFirstTouch, false);
+}, false);
 
 let navMenuIsLocked = false;
 
 navButton.addEventListener('mouseover', function(e) {
   if (!navMenuIsLocked) {
-    this.classList.toggle('menu-active');
+    this.classList.add('menu-active');
     let menuLinks = this.nextElementSibling;
     menuLinks.style.maxHeight = menuLinks.scrollHeight + 'px';
   }
 });
 
 navButton.addEventListener('click', function(e) {
-  navMenuIsLocked = !navMenuIsLocked
+  //Only lock menu open on click if user not using touchscreen
+  if (!window.USER_IS_TOUCHING) {
+    navMenuIsLocked = !navMenuIsLocked;
+  }
 });
 
 nav.addEventListener('mouseleave', function(e) {
